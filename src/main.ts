953112098c02e0b639832850cb5930c9793f6618
zhoucheng <zhoucheng133@outlook.com>
2026-05-10 23:25:07 +0800

    添加图标

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import { mount } from 'svelte';
+import './app.css';
+import App from './App.svelte';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const app = mount(App, {
   target: document.getElementById('app')!,
